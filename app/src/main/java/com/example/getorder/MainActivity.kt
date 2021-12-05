66696cb2dadd5e0eaeda9fa6d30b216145a8a0ac
Sergio Noriega <sergio.noriega@wiedii.co>
2021-12-05 15:18:51 -0500

    initial commit

--- app/src/main/java/com/example/getorder/MainActivity.kt
+++ app/src/main/java/com/example/getorder/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.getorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.getorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainActivity: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainActivity = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainActivity.root)
     }
 }
